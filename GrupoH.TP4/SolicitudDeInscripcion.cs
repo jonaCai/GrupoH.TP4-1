63879c687261dee08e1fe30848f6657c7500a0ba
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-29 11:57:59 -0300

    Creacion de Clases

---
+++ GrupoH.TP4/SolicitudDeInscripcion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoH.TP4
+{
+    class SolicitudDeInscripcion
+    {
+    }
+}
