7894941363dbe6ee1ab5efe5aea5246894cd2996
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-30 20:16:22 -0300

    Propiedades y constructor basico agregado

--- GrupoH.TP4/MateriasAlumno.cs
+++ GrupoH.TP4/MateriasAlumno.cs
@@ -1,6 +1,22 @@
-﻿namespace GrupoH.TP4
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoH.TP4
 {
     internal class MateriasAlumno
     {
+        public DateTime FechaDeCursada { get; internal set; }
+        public int Nota { get; internal set; }
+        public int CodigoMateria { get; internal set; }
+
+        public MateriasAlumno(DateTime fecha, int nota, int codMateria)
+        {
+            this.FechaDeCursada = fecha;
+            this.Nota = nota;
+            this.CodigoMateria = codMateria;
+        }
     }
 }
