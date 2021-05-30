5f0adc8087a69cd4e170fbcce66e307b2b880ae4
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-30 12:13:16 -0300

    Creados datos y constructor basico

--- GrupoH.TP4/Profesor.cs
+++ GrupoH.TP4/Profesor.cs
@@ -8,5 +8,21 @@ namespace GrupoH.TP4
 {
     class Profesor
     {
+        string Nombre { get; }
+        int Dni { get; }
+        string Mail { get; }
+        string Catedra { get; }
+        int Materia { get; }
+        int Curso { get; }
+
+        public Profesor(string nombre, int dni, string mail, string catedra, int materia, int curso)
+        {
+            this.Nombre = nombre;
+            this.Dni = dni;
+            this.Mail = mail;
+            this.Catedra = catedra;
+            this.Materia = materia;
+            this.Curso = curso;
+        }
     }
 }
