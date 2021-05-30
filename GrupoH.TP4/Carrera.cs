7aa75ae59085fded67cd0f862ae1c6570e994070
Juan Manuel Arroyo <juanma77@live.com.ar>
2021-05-30 12:45:15 -0300

    Datos y contructor basico

--- GrupoH.TP4/Carrera.cs
+++ GrupoH.TP4/Carrera.cs
@@ -8,5 +8,15 @@ namespace GrupoH.TP4
 {
     class Carrera
     {
-    }
+        public string Codigo { get; internal set; }
+        public string Nombre { get; internal set; }
+        public List<Materia> Materias { get; internal set; }
+
+        public Carrera(string codigo, string nombre, List<Materia> materias)
+        {
+            this.Codigo = codigo;
+            this.Nombre = nombre;
+            this.Materias = materias;
+        }
+}
 }
