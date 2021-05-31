6797c1203b95bcde5499c51188c027911d4e0650
Jonatán Ariel <jonariel@outlook.com>
2021-05-31 00:00:18 -0300

    Comentarios de pruebas borrado, uso de validador

--- GrupoH.TP4/Program.cs
+++ GrupoH.TP4/Program.cs
@@ -9,29 +9,20 @@ namespace GrupoH.TP4
     class Program
     {
         static void Main(string[] args)
-        {
-            int codigo_materia;
-
+        {          
             OfertaAcademica.CargarOferta();
             NominaAlumnos.CargarNomina();
             Carrera.CargarPlanesDeEstudios();
-            Console.Write("Codigo de la materia?:");
-            /*codigo_materia = int.Parse(Console.ReadLine());            
-            OfertaAcademica.MostrarOfertaCursos(codigo_materia);*/
 
-            
-            Console.ReadLine();
             
             const string menuPrincipal = "\tSistema de Inscripciones.";
             //const string declaracionJurada = "Ingrese ";
             int registro = 0;
             bool registroValido = false;
-            string opcionElegida;
-
+            
             while (registroValido == false)
             {
-                Console.WriteLine("Ingrese su Numero de Registro:");
-                int.TryParse(Console.ReadLine(), out registro);
+                registro = Validadores.NumeroPositivo("Ingrese su Numero de Registro:");
 
                 if (!NominaAlumnos.Inscriptos.ContainsKey(registro))
                 {
