922a62697f7d0d9c47beff3dd2f7d42f703f8bed
Jonatán Ariel <jonariel@outlook.com>
2021-06-09 14:25:22 -0300

    Agregado de validacion de alumnos que ya realizaron una solicitud de inscripcion

--- GrupoH.TP4/Program.cs
+++ GrupoH.TP4/Program.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace GrupoH.TP4
 {
     class Program
     {
         static void Main(string[] args)
-        {          
+        {
             OfertaAcademica.CargarOferta();
             NominaAlumnos.CargarNomina();
             Carrera.CargarPlanesDeEstudios();
 
-            
-            const string menuPrincipal = "Sistema de Inscripciones 1.0";           
+
+            const string menuPrincipal = "Sistema de Inscripciones 1.0";
             int registro = 0;
             bool registroValido = false;
-            
+
             while (registroValido == false)
             {
                 registro = Validadores.NumeroPositivo("Ingrese su Numero de Registro: ");
@@ -36,13 +36,28 @@ namespace GrupoH.TP4
 
             Console.WriteLine($"Bienvenido {NominaAlumnos.Inscriptos[registro].Nombre}! al {menuPrincipal} ");
             Console.WriteLine();
+            if (File.Exists("SolicitudesInscripcion.txt"))
+            {
+                using (var reader = new StreamReader("SolicitudesInscripcion.txt"))
+                {
 
-            new SolicitudDeInscripcion(registro);
-
-          
-
-
-                    
+                    while (!reader.EndOfStream)
+                    {
+                        var linea = reader.ReadLine();
+                        if (linea.Contains(registro.ToString()))
+                        {
+                            Console.WriteLine("Su numero de registro ya cuenta con una solicitud de inscripcion realizada.");
+                            Console.ReadKey();
+                            registroValido = false;
+                            break;
+                        }
+                        
+                    }
+                }
+                
+            }
+            if (registroValido == true) { new SolicitudDeInscripcion(registro); }
+            
         }
     }
 }
